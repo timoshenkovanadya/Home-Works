79e21bae6224780c09ea352e86b805bf620b9c5d
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-09-23 23:24:59 +0300

    refactor: make code shorter

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const input = document.querySelector('.input');
-const button = document.querySelector('.button-transform');
-const resultContainer = document.querySelector('.result-number')
+const input = document.querySelector(".input");
+const button = document.querySelector(".button-transform");
+const resultContainer = document.querySelector(".result-number");
 
-function BinaryTransformer (number) {
-    const clearNumber = parseInt(number, 10)
-    if (clearNumber >= 0) {
-        resultContainer.textContent = clearNumber.toString(2);
-      } else {
-        resultContainer.textContent= (clearNumber >>> 0).toString(2); 
-      }
-      
+function BinaryTransformer(number) {
+  const clearNumber = parseInt(number, 10);
+  resultContainer.textContent = (clearNumber >>> 0).toString(2);
 }
 
-button.addEventListener('click', ()=> BinaryTransformer(input.value))
+button.addEventListener("click", () => BinaryTransformer(input.value));
