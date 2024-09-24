851170945d09ed30a625aaad5ec7dfc34567142b
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-09-24 11:07:17 +0300

    feat: add console messages

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(0.1 + 0.2 === 3);
+console.log(((1,5 - 1) * 4) === 2);
+console.log(`${1===1}heartedness - one of the best human qualities`);
+console.log(('b'+'a'+ + 'a' + 'a').toLowerCase() === 'banana');
+console.log({} == {});
